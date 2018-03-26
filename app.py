6178ca1017389163d448e72e056866389ef6d25a
brfoley76 <brfoley76@gmail.com>
2018-03-25 19:12:11 -0700

    some new requirements

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request, redirect
+quandl.ApiConfig.api_key = 'oGV1c7rq87zKDJD27zat'
+from bokeh.plotting import figure, output_file, show
+from bokeh.embed import file_html
+from bokeh.resources import CDN
+import simplejson as json
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
-
-@app.route('/about')
-def about():
-  return render_template('about.html')
+  return render_template('stock_ticker_app.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
